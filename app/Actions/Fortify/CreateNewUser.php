6bc357ad7f6f62bf0297eb3310e3c4e399bedb41
Alexsander Matheus da Cruz Domingues <auzent14@gmail.com>
2026-06-10 03:35:20 -0300

    Mark new users' emails as verified
    Uncomment the call to $user->markEmailAsVerified() in the Fortify CreateNewUser action so newly created users are immediately marked as having verified email addresses. This skips the default email verification step upon registration.

--- app/Actions/Fortify/CreateNewUser.php
+++ app/Actions/Fortify/CreateNewUser.php
@@ -83,7 +83,7 @@ class CreateNewUser implements CreatesNewUsers
                 'email' => $input['email'],
                 'password' => Hash::make($input['password']),
             ]), function (User $user) {
-                // $user->markEmailAsVerified();
+                $user->markEmailAsVerified();
             });
         });
     }
